9f462565b4ea4a3bb41e201fdb832ad5813f502e
iamkumaranjana <134800985+toufic123@users.noreply.github.com>
2023-06-29 04:28:31 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "http://iamkumaranjana.rf.gd/graphql"
+	const endpoint = "https://iamkumaranjana.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://iamkumaranjana.rf.gd/` + encodeURI(path as string)
+					`https://iamkumaranjana.000webhostapp.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
